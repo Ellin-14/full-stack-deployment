be2313878bd0b5cfa03235ead2ef6eb5659bbb99
Ellin-14 <ellinjones14210809@gmail.com>
2026-01-22 22:27:05 +0530

    url add

--- server/src/main.py
+++ server/src/main.py
@@ -13,8 +13,9 @@ app = FastAPI()
 # ✅ CORS (safe for now, restrict later)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",  # local frontend
-    "https://full-stack-deployment-nmjt-7w9mi6rtf.vercel.app"],  # allow all during development
+    allow_origins=["http://localhost:3000",  # local dev
+        "https://full-stack-deployment-nmjt-8ex0adm3s.vercel.app" 
+    ],  # allow all during development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
